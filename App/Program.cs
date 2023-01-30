852b20cb7cf101d57d21bd1637bfb149c856d954
Aishik Bera <Aishik.Bera@clinisys.com>
2023-01-30 11:45:25 +0530

    Try-Catch-Finally

---
+++ App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                System.Console.WriteLine("Enter Integer : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("Enter 5 elements");
+                int[] Arr = new int[5];
+                    for(int i = 0;i<=5;i++)
+                    {
+                        Arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+            }
+            catch (FormatException ex0)
+            {
+               System.Console.WriteLine(ex0.Message);
+            }
+            catch (IndexOutOfRangeException ex1)
+            {
+                System.Console.WriteLine(ex1.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("Thank You");
+            }
+        }
+        
+    }
+} 
